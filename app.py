fc3145f5197c9e55f9b02178d5232ee67bfc6b28
Anand Atreya <64345749+Anandatreya@users.noreply.github.com>
2020-07-03 01:22:35 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,34 +1,39 @@
-#import BaseHTTPServer
-#import SimpleHTTPServer
-#import SocketServer
+import flask
+from flask import jsonify
 
-#PORT = 80
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
-#Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
-#httpd = SocketServer.TCPServer(("", PORT), Handler)
-print ("WElcome to My Udacity FInal Project..")
-#print ("serving at port", PORT)
-#httpd.serve_forever()
-
-
-
-
-
-
-#import BaseHTTPServer, SimpleHTTPServer
-#import http.server
-#import socketserver
-
-#PORT = 80
-#Handler = http.server.SimpleHTTPRequestHandler
-
-#with socketserver.TCPServer(("", PORT), Handler) as httpd:
-#        print("serving at port", PORT)
-#        httpd.serve_forever()
+# Create some test data for our catalog in the form of a list of dictionaries.
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
 
 
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
 
-#if __name__ == "__main__":
-        # load pretrained model as clf
-#                app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(books)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
+#app.run()
